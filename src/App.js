7fae8b7ccb9728ead91da196847c06f454c79c53
LAPTOP-B0DFG8D0\Pzhui <pangzhihui1220@gmail.com>
2022-03-28 15:06:18 +0800

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HookTimer from './component/HookTimer';
 import ParentComponent from './component/ParentComponent';
+import UserForm from './component/UserForm';
 
 function App() {
   return (
     <div className="App">
       {/* <ParentComponent /> */}
-      <HookTimer />
+      {/* <HookTimer /> */}
+      <UserForm />
     </div>
   );
 }
